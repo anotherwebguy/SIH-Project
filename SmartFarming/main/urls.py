a9d0e09bf861521273250670f52a8a819d86c99d
Mohit <mohitrajputms123@gmail.com>
2022-04-03 10:42:14 +0530

    changes

--- SmartFarming/main/urls.py
+++ SmartFarming/main/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('',views.welcome,name='welcome'),
     path('home/',views.index,name='index'),
     path('register/',views.register,name='register'),
-    path('signup/',views.signup,name='signup'),
-    path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
     path('signin/',views.signin,name='signin')
 ]
